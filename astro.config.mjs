ea769e8dd2107615624e188226f1842d51c12542
stschoelzel <info@stschoelzel.de>
2025-08-04 10:27:45 +0200

    boilerplate

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,5 @@
 // @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-	site: 'https://madsomeness.de',
-	integrations: [mdx(), sitemap()],
-	i18n: {
-    locales: ["de", "en"],
-    defaultLocale: "de",
-  }
-});
+export default defineConfig({});
